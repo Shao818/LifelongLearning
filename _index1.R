d47bf381f3e5fe91eca150980a5b0a0439c9c380
wang shao <wang_shao0818@163.com>
2021-04-08 10:07:22 +0800

    模块导航

---
+++ _index1.R
@@ -0,0 +1,24 @@
+---
+  menu:
+  header:
+  weight: 2
+title: 主页
+---
+
+
+
+  <center>
+  <a href="https://msg2020.pzhao.org/">
+  <img src="https://gitee.com/shao818/Figure/raw/master/%E6%9D%9C%E9%82%A6%E5%88%86%E6%9E%90%E5%9B%BE.png" style="zoom: 100%;" align="middle" alt="现代统计图形网站链接的二维码" />
+  </a>
+  </center>
+
+
+  > <font face="思源宋体 CN" >　　积土成山，风雨兴焉；积水成渊，蛟龙生焉；积善成德，而神明自得，圣心备焉。故不积跬步，无以至千里；不积小流，无以成江海。骐骥一跃，不能十步；驽马十驾，功在不舍。锲而舍之，朽木不折；锲而不舍，金石可镂。蚓无爪牙之利，筋骨之强，上食埃土，下饮黄泉，用心一也。蟹六跪而二螯，非蛇鳝之穴无可寄托者，用心躁也。
+
+>　　　　　　　　　　　　　　　　　　　　　　　　　　　　　          　　**<font size=4 >——《劝学》　荀子·先秦</font>**
+
+
+
+  </font>
+
